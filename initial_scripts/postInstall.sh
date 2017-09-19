5986a5653b273acabf870c007ce10a854b72883a
prassaschr <prassaschr@gmail.com>
2017-09-19 21:31:31 +0300

    Update postInstall.sh

--- initial_scripts/postInstall.sh
+++ initial_scripts/postInstall.sh
@@ -7,3 +7,5 @@ apt-get update
 apt-get upgrade
 apt-get install -y rsync ssh openssh-client openssh-server curl libcurl3 libopts25 ntp ntp-doc fail2ban python3-pyinotify python3-systemd whois  attr libaio1 libavahi-client3 libavahi-common-data libavahi-common3 libcups2 libfile-copy-recursive-perl libgpgme11 libldb1 libpython2.7 libtalloc2 libtdb1 libtevent0 libwbclient0 python-crypto python-dnspython python-ldb python-samba python-talloc python-tdb samba samba-common samba-common-bin samba-dsdb-modules samba-libs samba-vfs-modules tdb-tools update-inetd libarchive13 liblzo2-2  libsmbclientpython-samba smbclient zip unzip python-talloc cifs-utils fuse libntfs-3g871 ntfs-3g apt-transport-https webmin
 echo "PermitRootLogin yes" | tee -a /etc/ssh/sshd_config
+# Delete me
+rm $0
