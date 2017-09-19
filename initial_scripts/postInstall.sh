73f1689993814dc22c1423f1132e9f21ad06fdb8
prassaschr <prassaschr@gmail.com>
2017-09-19 08:35:25 +0300

    v 0.2

--- initial_scripts/postInstall.sh
+++ initial_scripts/postInstall.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 echo "deb http://download.webmin.com/download/repository sarge contrib" | tee -a /etc/apt/sources.list
+cd /root
 wget http://www.webmin.com/jcameron-key.asc
-apt-key add /tmp/jcameron-key.asc #Download the script to /tmp and all the files to tmp
+apt-key add /root/jcameron-key.asc #Download the script to /tmp and all the files to tmp
 apt-get update
 apt-get upgrade
 sudo apt-get install -y rsync ssh openssh-client openssh-server curl libcurl3 libopts25 ntp ntp-doc fail2ban python3-pyinotify python3-systemd whois  attr libaio1 libavahi-client3 libavahi-common-data libavahi-common3 libcups2 libfile-copy-recursive-perl libgpgme11 libldb1 libpython2.7 libtalloc2 libtdb1 libtevent0 libwbclient0 python-crypto python-dnspython python-ldb python-samba python-talloc python-tdb samba samba-common samba-common-bin samba-dsdb-modules samba-libs samba-vfs-modules tdb-tools update-inetd libarchive13 liblzo2-2  libsmbclientpython-samba smbclient zip unzip python-talloc cifs-utils fuse libntfs-3g871 ntfs-3g apt-transport-https webmin
