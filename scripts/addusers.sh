a5ce0369364ac9163c6310bf1fa6850ba89cf12b
prassaschr <prassaschr@gmail.com>
2017-09-22 13:42:47 +0300

    correct

--- scripts/addusers.sh
+++ scripts/addusers.sh
@@ -60,7 +60,8 @@ else
     #This creates the password for the user suppresses output of passwd
     #The -p in the useradd function doesn't set it properly
     echo "${userid[$x]}" | passwd -q "$user" > /dev/null
-	echo "${userid[$x]}" | smbpasswd -a -s "$user" > /dev/null
+	echo -ne "${userid[$x]}\n${userid[$x]}\n" | smbpasswd -a -s "$user" > /dev/null
+	#echo -ne "$PASS\n$PASS\n" | smbpasswd -a -s $LOGIN
 	smbpasswd -e $user > /dev/null
     #sends mail to user
     echo "Welcome! Your account has been created.  Your username is $user and temporary password is \"$password\" without the quotes." | mail -s "New Account for $user" -b root $user
